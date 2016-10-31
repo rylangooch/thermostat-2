d6da86c4166179c4c77655d28c66723746af03db
rylan <rylangooch@gmail.com>
2016-10-31 17:05:05 +0000

    Added temperature incerase and decrease as well as minimum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,6 +9,18 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases with temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+  for (var i = 0; i < 11; i++) {
+    thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function Thermostat() {
+  this.MINIMUM_TEMPERATURE = 10
   this.temperature = 20;
 };
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+}
